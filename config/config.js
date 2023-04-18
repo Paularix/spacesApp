4823a63fa62e0145095319b9d8faee9c8ff5a613
Alejandro Velardo <a.velardo.lucero@gmail.com>
2023-04-18 10:38:37 +0200

    Algunos cambios en index.js para usar morgan como dependencia de desarrollo, crear config y conexion a DB y creación de los primeros routers.

---
+++ config/config.js
@@ -0,0 +1,12 @@
+// Data base configuration  
+
+export default {
+
+    "username": "root",
+    //"password": "123456789",
+    //"database": "simple_ecommerce",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  
+  }
+  
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Librerias que se necesitan en el proyecto
-import express from "express"
-import cors from "cors"
+import express from 'express'
+import cors from 'cors'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
+
+import userRouter from './routes/userRouter.js'
+import indexRouter from './routes/indexRouter.js'
+
 
 // dotenv.config()  sirve para poder usar las variables de entorno en el archivo .env  
 dotenv.config()
@@ -13,14 +17,17 @@ const app = express()
 // Middleware para poder procesar los datos JSON, evitar los errores de CORS, y tener información sobre las requests en terminal.
 app.use(express.json())
 app.use(cors())
-app.use(morgan((tokens, req, res) => {
-    return `${req.method} ${req.url} ${res.statusCode} - ${tokens['response-time'](req, res)} ms`;
-}))
 
-// Primer endpoint para verificar que el servidor esta levantado.
-app.get("/", (req, res) => {
-    res.send("Hello, this is SpacesApp Server!")
-})
+const env = process.env.NODE_ENV || 'development'
+if (env === 'development') {
+    app.use(morgan((tokens, req, res) => {
+        return `${req.method} ${req.url} ${res.statusCode} - ${tokens['response-time'](req, res)} ms`;
+    }))
+}
+
+// Routers
+app.use('/api', indexRouter)
+app.use('/api/users', userRouter)
 
 
 //Montando la app en el servidor (utilizando la variable de entorno, "process.env")
