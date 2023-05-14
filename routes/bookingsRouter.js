ad653bc203e7bf7e29cee18abeeaec28da15ad24
Alejandro Velardo <a.velardo.lucero@gmail.com>
2023-05-14 21:16:07 +0200

    Pantalla de reseervas para owner terminada

--- routes/bookingsRouter.js
+++ routes/bookingsRouter.js
@@ -186,5 +186,107 @@ router.get("/auth/Myreservations", [authenticate, authError], (req, res) => {
     }
 })
 
+// GET información protegida de las reservas del usuario
+// @desc ruta protegida perfil de usuario
+router.get("/auth/bookingmgmt", [authenticate, authError], (req, res) => {
+    const token = req.headers.authorization || ''
+    if (token) {
+        const decoded = jsonwebtoken.decode(token)
+        sequelize.sync().then(() => {
+            Bookings.findAll({
+                include: [{
+                    model: Spaces,
+                    where: {rid_host_user: decoded.id}  ,
+                    required: true
+                },{
+                    model: Users,
+                    required: true
+                }]
+            })
+                .then(bookings => {
+                    res.status(200).json({
+                        ok: true,
+                        data: bookings
+                    })
+                })
+                .catch((error) => {
+                    console.log('FALLA', error)
+                    res.status(400).json({
+                        ok: false,
+                        error
+                    })
+                })
+        })
+            .catch((error) => {
+                console.log('FALLA', error)
+                res.status(400).json({
+                    ok: false,
+                    error
+                })
+            })
+
+    }
+})
+
+// GET información protegida de las reservas del usuario
+// @desc ruta protegida perfil de usuario
+router.put("/auth/bookingmgmt/accept/:id", [authenticate, authError], (req, res) => {
+    const token = req.headers.authorization || ''
+    if (token) {
+        const decoded = jsonwebtoken.decode(token)
+        sequelize.sync().then(() => {
+            Bookings.findOne({ where: { id: req.params.id } })
+                .then(bookings_trobat =>
+                    bookings_trobat.update({status: 1})
+                )
+                .then(bookings_modificat => res.json({
+                    ok: true,
+                    data: bookings_modificat
+                }))
+                .catch(error => res.json({
+                    ok: false,
+                    error: error.message
+                }));
+    
+        }).catch((error) => {
+            res.json({
+                ok: false,
+                error: error.message
+            })
+        });
+
+    }
+})
+
+
+// GET información protegida de las reservas del usuario
+// @desc ruta protegida perfil de usuario
+router.put("/auth/bookingmgmt/reject/:id", [authenticate, authError], (req, res) => {
+    const token = req.headers.authorization || ''
+    if (token) {
+        const decoded = jsonwebtoken.decode(token)
+        sequelize.sync().then(() => {
+            Bookings.findOne({ where: { id: req.params.id } })
+                .then(bookings_trobat =>
+                    bookings_trobat.update({status: 2})
+                )
+                .then(bookings_modificat => res.json({
+                    ok: true,
+                    data: bookings_modificat
+                }))
+                .catch(error => res.json({
+                    ok: false,
+                    error: error.message
+                }));
+    
+        }).catch((error) => {
+            res.json({
+                ok: false,
+                error: error.message
+            })
+        });
+
+    }
+})
 export default router;
 
