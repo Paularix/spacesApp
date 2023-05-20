1a7eebd638236e8e1ea3c452466c4faed5490ac0
ricard <ricard.herbamel@gmail.com>
2023-05-20 23:58:27 +0200

    deploy 2

--- frontSpaces/src/apiconfig.js
+++ frontSpaces/src/apiconfig.js
@@ -1,3 +1,6 @@
-export const API_URL = "http://localhost:3080/api/";
-export const BASE_URL = "http://localhost:3080/";
+//export const API_URL = "http://localhost:3080/api/";
+//export const BASE_URL = "http://localhost:3080/";
+
+export const API_URL = "/api/";
+export const BASE_URL = "/";
 
